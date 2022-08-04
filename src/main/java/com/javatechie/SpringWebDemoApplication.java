f69dd490adbd4172ad756f08d860fa16f47a907a
Saurabh <saurabh.sb83@gmail.com>
2022-08-04 09:43:13 +0530

    initial commit

---
+++ src/main/java/com/javatechie/SpringWebDemoApplication.java
@@ -0,0 +1,15 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringWebDemoApplication.class, args);
+		System.out.println("Hello World !");
+	}
+
+}
